49d038c4603b0e458d29685a8aa8967b56772a37
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-09-25 20:03:41 +0530

    Fix: admin start command

--- handlers.py
+++ handlers.py
@@ -1,15 +1,16 @@
 from hydrogram.types import Message
-from hydrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 from db import add_coupon, fetch_latest_coupon,update_coupon,fetch_coupon_count ,clear_all_redeemed_coupons, clear_all_unredeemed_coupons,clear_all_coupons
 
 
 async def admin_start_handler(client, message:Message):
     text = 'Hi! Admin  \n\n \
+Total redeemed coupons: {coupons_count["redeemed"]}  \n \
+Total unredeemed coupons:  {coupons_count["unredeemed"]} \n\n \
 All available commands    \n \
     /clear_redeemed - Clear redeemed coupons \n \
     /clear_unredeemed - Clear unredeemed coupons \n  \
-    /clear_all - Clear all coupons \n \
-    /display_all - Display all coupons \n'
+    /clear_all - Clear all coupons \n \ '
+    # /display_all - Display all coupons \n'
     await message.reply(text=text)
     
 async def clear_redeemed_handler(client, message:Message):
@@ -25,12 +26,10 @@ async def clear_all_handler (client, message:Message):
     await clear_all_coupons()
     await message.reply(text="All coupons cleared!")
 
-async def display_all_handler (client, message:Message):
-    coupons_count = await fetch_coupon_count()
-    text = f'Welcome to the giveaway bot! \n \
-Total redeemed coupons: {coupons_count["redeemed"]}  \n \
-Total unredeemed coupons:  {coupons_count["unredeemed"]}'
-    await message.reply(text=text)
+# async def display_all_handler (client, message:Message):
+#     coupons_count = await fetch_coupon_count()
+#     text = f'Welcome to the giveaway bot! '
+#     await message.reply(text=text)
 
 async def listen_handler(client, message:Message):
     # save coupons to database
