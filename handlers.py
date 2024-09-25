7a97f5e3b1cc03b58beed20f3f2513eeef3e8fea
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2024-09-25 20:34:03 +0530

    feat: file support added as coupon

--- handlers.py
+++ handlers.py
@@ -3,13 +3,14 @@ from db import add_coupon, fetch_latest_coupon,update_coupon,fetch_coupon_count 
 
 
 async def admin_start_handler(client, message:Message):
-    text = 'Hi! Admin  \n\n \
+    coupons_count = await fetch_coupon_count()
+    text = f'Hi! Admin  \n\n \
 Total redeemed coupons: {coupons_count["redeemed"]}  \n \
 Total unredeemed coupons:  {coupons_count["unredeemed"]} \n\n \
 All available commands    \n \
     /clear_redeemed - Clear redeemed coupons \n \
     /clear_unredeemed - Clear unredeemed coupons \n  \
-    /clear_all - Clear all coupons \n \ '
+    /clear_all - Clear all coupons '
     # /display_all - Display all coupons \n'
     await message.reply(text=text)
     
@@ -32,14 +33,28 @@ async def clear_all_handler (client, message:Message):
 #     await message.reply(text=text)
 
 async def listen_handler(client, message:Message):
+    file_id = None
+    text = message.text
+    if message.document:
+        file_id = message.document.file_id
+        text = message.caption
+    if message.media:
+        text = message.caption
     # save coupons to database
-    coupon = {"coupon":message.text , "redeemed":False}
+    coupon = {"text":text,"file_id":file_id , "redeemed":False}
     return await add_coupon(coupon)
 
 async def giveaway_handler(client,message:Message):
     # fetch the latest coupon from db
     unredeemed_coupon = await fetch_latest_coupon()
     if not unredeemed_coupon: return await message.reply(text="All coupons already redeemed!")
+    text = unredeemed_coupon.get("text") or  " "
+    file_id = unredeemed_coupon.get("file_id")
+    if file_id:
+        print("File id found")
+        await message.reply_document(document=file_id, caption=text)
+        return await update_coupon(unredeemed_coupon,True)
+    await message.reply(text=text)
     await update_coupon(unredeemed_coupon,True)
-    return await message.reply(text=unredeemed_coupon.get("coupon"))
+
     
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from hydrogram import Client , filters, idle
 from hydrogram.handlers import MessageHandler
 from config import api_id, api_hash, bot_token, Channel_chat_id, admins
 from handlers import giveaway_handler,listen_handler
-from handlers import admin_start_handler , clear_all_handler,clear_redeemed_handler,clear_unredeemed_handler,display_all_handler
+from handlers import admin_start_handler , clear_all_handler,clear_redeemed_handler,clear_unredeemed_handler
 from db import create_db_if_not_exists
 
 bot_client = Client("giveaway_bot",api_id=api_id,api_hash=api_hash,bot_token=bot_token)
@@ -19,7 +19,6 @@ async def main():
     bot_client.add_handler(MessageHandler(clear_redeemed_handler,filters.command("clear_redeemed") & filters.user(admins) ))
     bot_client.add_handler(MessageHandler(clear_unredeemed_handler, filters.command("clear_unredeemed") & filters.user(admins) ))
     bot_client.add_handler(MessageHandler(clear_all_handler, filters.command("clear_all") & filters.user(admins)))
-    bot_client.add_handler(MessageHandler(display_all_handler, filters.command("display_all") & filters.user(admins)))
 
     
     ## Non admin commands
